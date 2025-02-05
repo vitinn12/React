331bab022c6c995c19693cc3c7cb8d9eb8c5fa57
vitinn12 <vitoralves1212.va@gmail.com>
2025-02-05 16:11:19 -0300

    w

--- projeto-react/react/src/components/SaveUser/index.jsx
+++ projeto-react/react/src/components/SaveUser/index.jsx
@@ -1,9 +1,30 @@
 
-// import { Button, TextField } from "@mui/material";
-// import { useState } from "react";
+import { Button, TextField } from "@mui/material";
+import { useState } from "react";
 // import { useNavigate } from "react-router-dom";
 
 export const SaveUser = () => {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [cpf, setCpf] = useState('');
+  const [telefone, seTelefone] = useState('');
+  const [senha, setSenha] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const response = await fetch('http://localhost:3000/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ nome, email, cpf, telefone, senha }),
+    });
+
+    const data = await response.json();
+    alert(data.message);
+
+  };
   // const navigate = useNavigate();
 
   // function Home(){
@@ -12,45 +33,27 @@ export const SaveUser = () => {
 
   return (
     <section>
-        <form action="http://localhost:3000/signup" method="post">
+        <form onSubmit={handleSubmit}>
 
-        {/* <h1>Cadastrar Usuário</h1>
+        <h1>Cadastrar Usuário</h1>
         <nav>
           <TextField
             label="Nome"
             variant="outlined"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+            required
           />
           <TextField
             label="Email"
             variant="outlined"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            required
           />
-        </nav> */}
+        </nav>
+        <button type="submit">Enviar</button>
 
-            <div>
-                Nome: <input type="text" name="nome"/>
-
-            </div>
-            
-            <div>
-                CPF: <input type="text" name="cpf"/>
-
-            </div>
-            <div>
-                Telefone: <input type="text" name="telefone"/>
-            </div>
-            <div>
-                E-mail: <input type="email" name="email"/>
-            </div>
-            <div>
-                Senha: <input type="password" name="senha"/>
-            </div>
-            <div>
-                <input type="submit" value="Enviar"/>
-            </div>
         </form>
     </section>
   );
